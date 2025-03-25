21ed13019556fd066a650e3541d069a2532188ae
AdhritShetty <adhritshetty2009@gmail.com>
2025-03-25 16:06:28 +0000

    Variables

---
+++ variable.py
@@ -0,0 +1,12 @@
+#creating dofferent variables
+x=5
+y="John"
+print(x)
+print(y)
+
+codingal="codingal"
+print(codingal)
+
+#user input
+name= input ("enter your name:")
+print("\nhello",name,"\nwelcome to codingal")
