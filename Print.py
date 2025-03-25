429b78e46c6c5da225e2886c248b01dcea2c30ac
AdhritShetty <adhritshetty2009@gmail.com>
2025-03-25 15:51:26 +0000

    Print Statement

---
+++ Print.py
@@ -0,0 +1,14 @@
+#print a sentence 
+print ("welecome to the world of python")
+
+#print a number
+print (7)
+
+#next line after print 
+print ("hello python\n")
+
+#pass multiple arguements for printing
+print ("hello",5)
+
+# end arguement with a print of statement
+print ("welcome to",end="+")
