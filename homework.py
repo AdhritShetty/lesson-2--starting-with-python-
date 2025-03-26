e1008d2f2bbe38cac6279ee3864e89df8d302c62
AdhritShetty <adhritshetty2009@gmail.com>
2025-03-26 07:41:53 +0000

    homework

---
+++ homework.py
@@ -0,0 +1,10 @@
+print("Storing the birth dates of my family")
+#print the birthday dates of family members
+Adhrit="Adhrit=13 Feb,2009"
+print(Adhrit)
+Adhwit="Adhwit=15 Dec,2014"
+print(Adhwit)
+Raksha="Raksha=13 feb,1984"
+print(Raksha)
+Gangadhar="Gangadhar=5 September,1979"
+print(Gangadhar)
